073f8ce480da0ec35d9fdcac422d85fe8176d061
AndreSS-ntp <79152902+AndreSS-ntp@users.noreply.github.com>
2026-05-27 00:24:34 +0300

    сделать endDate указателем для правильной работы с бд

--- sub-aggregator-service/internal/domain/domain.go
+++ sub-aggregator-service/internal/domain/domain.go
@@ -12,7 +12,7 @@ type Subscription struct {
 	Price       int64
 	UserID      uuid.UUID
 	StartDate   time.Time
-	EndDate     time.Time
+	EndDate     *time.Time
 }
 
 type SubscriptionDTO struct {
@@ -21,22 +21,16 @@ type SubscriptionDTO struct {
 	Price       int64     `json:"price"`
 	UserID      uuid.UUID `json:"user_id"`
 	StartDate   string    `json:"start_date"`
-	EndDate     string    `json:"end_date"`
+	EndDate     *string   `json:"end_date"`
 }
 
 func ToSubscriptionDTO(sub *Subscription) *SubscriptionDTO {
-	startYear, startMonth, _ := sub.StartDate.Date()
-	endYear, endMonth, _ := sub.EndDate.Date()
-
-	startDate := fmt.Sprintf("%d-%d", startMonth, startYear)
-	endDate := fmt.Sprintf("%d-%d", endMonth, endYear)
-	// если месяц без ведущего ноля
-	if startMonth < 10 {
-		startDate = "0" + startDate
+	var endDate *string
+	if sub.EndDate != nil {
+		s := sub.EndDate.Format("01-2006")
+		endDate = &s
 	}
-	if endMonth < 10 {
-		endDate = "0" + endDate
-	}
+	startDate := sub.StartDate.Format("01-2006")
 
 	return &SubscriptionDTO{
 		ID:          sub.ID,
@@ -58,13 +52,18 @@ func ToSubscription(dto *SubscriptionDTO) (*Subscription, error) {
 	if err != nil {
 		return nil, fmt.Errorf("parse date from string: %w", err)
 	}
-	if len(dto.EndDate) != 7 {
-		dto.EndDate = "0" + dto.EndDate
+
+	var endDate *time.Time
+	if dto.EndDate != nil {
+		if len(*dto.EndDate) != 7 {
+			*dto.EndDate = "0" + *dto.EndDate
+		}
+		*endDate, err = time.Parse(dateFormat, *dto.EndDate)
+		if err != nil {
+			return nil, fmt.Errorf("parse date from string: %w", err)
+		}
 	}
-	endDate, err := time.Parse(dateFormat, dto.EndDate)
-	if err != nil {
-		return nil, fmt.Errorf("parse date from string: %w", err)
-	}
+
 	return &Subscription{
 		ID:          dto.ID,
 		ServiceName: dto.ServiceName,
