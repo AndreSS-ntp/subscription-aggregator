7ab36eb9b4a270621785be4a1fd3856205bce67a
AndreSS-ntp <79152902+AndreSS-ntp@users.noreply.github.com>
2026-05-26 20:11:28 +0300

    добавить миграцию создания таблицы subscriptions

---
+++ sub-aggregator-service/migrations/20260526155829_create_subscriptions_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS subscriptions (
+    id UUID PRIMARY KEY,
+    service_name TEXT NOT NULL,
+    price BIGINT NOT NULL,
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS items;
+-- +goose StatementEnd
